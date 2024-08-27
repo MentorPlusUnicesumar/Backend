4001d1b85db5366f59bb59c0f68909c80c64c905
Gabriel Prisco_N <gabrielmarcosprisco@gmail.com>
2024-08-26 21:42:19 -0300

    Configurando email para envio via smtp

--- src/configs/mailer.config.ts
+++ src/configs/mailer.config.ts
@@ -11,5 +11,19 @@ export const mailerConfig: MailerOptions = {
       layoutsDir: path.resolve(__dirname, '..', '..', 'templates'),
     },
   },
-  transport: process.env.EMAIL_TRANSPORT,
+  transport: {
+    host: 'smtp.office365.com',
+    port: 587,
+    secure: false, // true para 465, false para outras portas
+    auth: {
+      user: 'atendimento.mentorplus@hotmail.com',
+      pass: 'mentorado123',
+    },
+    tls: {
+      rejectUnauthorized: false, // Pode ser necess�rio para alguns servidores
+    },
+  },
+  defaults: {
+    from: '"No Reply" <atendimento.mentorplus@hotmail.com>', // Endere�o de e-mail padr�o
+  },
 };
