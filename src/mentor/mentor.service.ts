b38db8df23f0fa8666f55a6a38ede8f3f15f7609
Gabriel Prisco_N <gabrielmarcosprisco@gmail.com>
2024-10-08 19:57:10 -0300

    #22303 - Arrumando code owners

--- src/mentor/mentor.service.ts
+++ src/mentor/mentor.service.ts
@@ -3,6 +3,7 @@ import { CreateMentorDto } from './dto/create-mentor.dto';
 import { UpdateMentorDto } from './dto/update-mentor.dto';
 import { UsersService } from 'src/users/users.service';
 
+
 @Injectable()
 export class MentorService extends UsersService{
 
