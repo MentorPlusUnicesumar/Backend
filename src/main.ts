2c348619ec014388c6f22b55917fb59e212ac017
Gabriel Prisco <gabrielmarcosprisco@gmail.com>
2024-11-14 19:03:35 -0300

    #23200 - Alterando SWAGGER para /docs

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
     )
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('docs', app, documentFactory);
 
   await app.listen(3000);
 }
